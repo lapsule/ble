bc59376e2a9455def9420962a78252a9cd4127f6
Fu Tilong <fuacici@gmail.com>
2013-11-10 23:22:03 +0800

    tt

---
+++ ble-utility/BLAvailableServicesViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLAvailableServicesViewController.h
+//  ble-utility
+//
+//  Created by Joost Fu on 11/10/13.
+//  Copyright (c) 2013 joost. All rights reserved.
+//
+
+#import "BLTableViewController.h"
+
+@interface BLAvailableServicesViewController : BLTableViewController
+
+@end
---
+++ ble-utility/BLAvailableServicesViewController.m
@@ -0,0 +1,38 @@
+//
+//  BLAvailableServicesViewController.m
+//  ble-utility
+//
+//  Created by Joost Fu on 11/10/13.
+//  Copyright (c) 2013 joost. All rights reserved.
+//
+
+#import "BLAvailableServicesViewController.h"
+
+@interface BLAvailableServicesViewController ()
+
+@end
+
+@implementation BLAvailableServicesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

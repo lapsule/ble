f1403e2ac83a76632e9ef7f6b3b91fab2aa895a7
Joost Fu <futilong@bitauto.com>
2013-11-13 15:54:14 +0800

    83 cache

--- ble-utility/BLAppDelegate.h
+++ ble-utility/BLAppDelegate.h
@@ -11,5 +11,4 @@
 @interface BLAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 @end
--- ble-utility/BLAppDelegate.m
+++ ble-utility/BLAppDelegate.m
@@ -7,11 +7,16 @@
 //
 
 #import "BLAppDelegate.h"
-
+#import "GDataXMLNode.h"
+@interface BLAppDelegate ()
+@property (nonatomic,strong) NSMutableDictionary * uuidNames;
+@property (nonatomic,strong) NSString * uuidPlistPath;
+@end
 @implementation BLAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self loadNames];
     // Override point for customization after application launch.
     return YES;
 }
@@ -42,5 +47,53 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+#pragma mark - 
+- (NSString *) uuidPlistPath
+{
+    if (!_uuidPlistPath)
+    {
+        NSArray * dirs = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+        NSString * file = [dirs[0] stringByAppendingPathComponent:@"uuid_names.plist"];
+        self.uuidPlistPath = file;
+    }
+    return _uuidPlistPath;
+}
+- (void)loadNames
+{
 
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[self uuidPlistPath]])
+    {
+        self.uuidNames = [NSMutableDictionary dictionaryWithContentsOfFile:[self uuidPlistPath]];
+    }
+    if (self.uuidNames == nil|| self.uuidNames.count==0)
+    {
+        [self parseNames];
+        [self.uuidNames writeToFile:self.uuidPlistPath atomically:YES];
+    }
+}
+- (void)parseNames
+{
+    self.uuidNames = [NSMutableDictionary dictionaryWithCapacity:50];
+    NSString * folder = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"servicesdefs"];
+    NSFileManager * filer = [NSFileManager defaultManager];
+    NSDirectoryEnumerator * enumerator =  [filer enumeratorAtPath: folder];
+    NSString * item = nil;
+    while (item = [enumerator nextObject])
+    {
+        NSData * data = [NSData dataWithContentsOfFile:[folder stringByAppendingPathComponent:item]];
+        GDataXMLDocument * doc = [[GDataXMLDocument alloc] initWithData:data error:nil];
+        if (doc)
+        {
+            GDataXMLElement * servicexml = [doc rootElement];
+            NSMutableDictionary * dict = [NSMutableDictionary dictionaryWithCapacity:10];
+            for (GDataXMLNode * att in servicexml.attributes)
+            {
+                dict[att.name] = [att.children[0] XMLString];
+            }
+            dict[@"CBObject"] = servicexml.name;
+            self.uuidNames[dict[@"uuid"]]=dict;
+        }
+    }
+    
+}
 @end
